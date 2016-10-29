1f96c828859280e92428a0ad8e7307779f4978c3
winterwalk <jesterfourzero@gmail.com>
2016-10-29 08:14:35 -0400

    lol u mad bro

--- lc.py
+++ lc.py
@@ -1,6 +1,5 @@
 '''
 This is the newest version of lc.py.
-
 '''
 
 import matplotlib.pyplot as plt
@@ -29,15 +28,27 @@ def gauss (x, a, b, c):
    return a*np.exp(-(x-b)**2/(2*c**2))
 
 # Notable constants, arrays declared here:
+
 P = 1.76358757 #OrbitalPeriod
 FirstTimeValue = timeList[0]
 FirstCentroid = 0.42
-NumberOfPeriods = int(np.ceil(((timeList[-1])-(timeList[0]))/ P))
+
+# NPeriods is exactly what it looks like. By taking the ceiling function
+# of the total amount of time in the range divided by the period, you
+# effectively get the number of periods in a given range. Ceiling works
+# better than floor because you'd prefer an overestimate over lost data.
+
+NPeriods = int(np.ceil(((timeList[-1])-(timeList[0]))/ P))
 timeArray = np.array(timeList)
+
+# Decrements each value in timeArray by the first element's value.
+
 timeArray = timeArray - timeArray[0]
+
 print(timeArray[0])
 fluxArray = np.array(fluxList)
-for i in range(0, NumberOfPeriods):
+
+for i in range(0, NPeriods):
     found = np.where((timeArray > i * P) & (timeArray < (i + 1) * P))
     croppedTimeArray = timeArray[found]
     croppedFluxArray = fluxArray[found]
