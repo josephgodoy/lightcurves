b7777269c707c06757c2d15a557a4724c8fedddb
winterwalk <jesterfourzero@gmail.com>
2016-11-21 22:16:35 -0500

    Added file output for primary.py.

--- primary.py
+++ primary.py
@@ -1,11 +1,10 @@
 '''
 ================= PRIMARY.PY ===============================
-Update: Solved the empty list problem by testing
-whether or not the length of croppedTimeArray is nonzero.
-If it is nonzero, the function fitting loop runs, if not,
-the dataset is skipped.
-Update #2: The fit parameters are now stored in an array,
-fitParameters. It contains the results of each loop's popt.
+Latest update: Primary output is now written to a whitespace-delimited
+file of the following three-column format:
+Orbit Number | Centroid Location | Uncertainty.
+Slices with no data in them will have values of -99.
+Slices with unreliable data (uncertainty > 1) will have values of -98.
 ============================================================
 '''
 
@@ -14,70 +13,73 @@ import numpy as np
 from math import floor
 from scipy.optimize import curve_fit
 
+#I/O preparation:
+
 datafile = open ('kepler.dat')
+output = open('LCP-output.txt', 'w')
 kdata = datafile.readlines()
 datafile.close()
-
 timeList = []
 fluxList = []
 
-# Populates lists with file data:
+# Populates lists:
 
 for line in kdata:
     ph = line.split()
     timeList.append(float(ph[0]))
     fluxList.append(float(ph[1]))
-print("len(timeList) = ", len(timeList))
 
 # Gaussian function definition:
 
 def gauss (x, a, b, c):
    return a*np.exp(-(x-b)**2/(2*c**2))
 
-# Notable constants, arrays declared here:
+# Notable constants and arrays:
 
-P = 1.76358757 #OrbitalPeriod
+P = 1.76358757 # Orbital period in days.
 FirstTimeValue = timeList[0]
 FirstCentroid = 0.42
+w = 0.3   # Width of cropping region. Do NOT use in the Gaussian.
+eclipseTime = 1.3
 fitParameters = []
-z = 0
-
-'''
-# Remember: The fit parameters array stores parameters in threes.
-# i, i+1, and i+2 counts as one set of popt[] values.
-#
-# NPeriods is exactly what it looks like. By taking the ceiling function
-# of the total amount of time in the range divided by the period, you
-# effectively get the number of periods in a given range. Ceiling works
-# better than floor because you'd prefer an overestimate over lost data.
-'''
 NPeriods = int(np.ceil(((timeList[-1])-(timeList[0]))/ P))
 timeArray = np.array(timeList)
-
-# Decrements each value in timeArray by the first element's value.
-
-timeArray = timeArray - timeArray[0]
-print(timeArray[0])
 fluxArray = np.array(fluxList)
-w = 0.3 #width
-eclipseTime = 1.3
+timeArray = timeArray - timeArray[0]
+
+# The magic happens here:
+
 for i in range(0, NPeriods):
+
     found = np.where((timeArray > i * P) & (timeArray < (i + 1) * P))
-    #found = np.where((timeArray > (i * P + eclipseTime ) - w) & (timeArray < (i * P + eclipseTime ) + w))
     croppedTimeArray = timeArray[found]
     croppedFluxArray = fluxArray[found]
-    p0 = [-5, (FirstCentroid + (i * P)), 0.3]
+    p0 = [-5, (FirstCentroid + (i * P)), 0.3] # Depth-centroid-width p0 array.
     print(croppedTimeArray)
+
     if len(croppedTimeArray) != 0:
+
         print("i = %i" % i)
+
         plt.plot(croppedTimeArray, croppedFluxArray, 'o')
         popt, pcov = curve_fit (gauss, croppedTimeArray, croppedFluxArray, p0, maxfev = 100000)
-        fitParameters.append(popt[z])
-        fitParameters.append(popt[z+1])
-        fitParameters.append(popt[z+2])
-        z = 0
-        print("centroid = ", popt[1], "+/- ", np.sqrt(pcov[1,1]))
-        print("guess = ", p0)
+        fitParameters.append(popt[0])
+        fitParameters.append(popt[1])
+        fitParameters.append(popt[2])
+
+        print("Centroid = ", popt[1], "+/- ", np.sqrt(pcov[1,1]))
+        print("Guess = ", p0)
+
+        if np.sqrt(pcov[1,1]) > 1:
+           output.write("%i %f %f \n" % (i, -98, -98))
+        else:
+           output.write("%i %f %f \n" % (i, popt[1], np.sqrt(pcov[1,1])))
+
         plt.plot(croppedTimeArray, gauss(croppedTimeArray, *popt))
-        plt.show()
-        print(fitParameters)
+        #plt.show()
+
+    elif len(croppedTimeArray) == 0:
+        output.write("%i %f %f \n" % (i, -99, -99))
+
+print(fitParameters)
+output.close()
