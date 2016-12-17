970bfba91d825ac6b9f1f6a7e91b4a66e7d95537
winterwalk <jesterfourzero@gmail.com>
2016-12-17 17:46:10 -0500

    New-and-improved Bash script.

--- lightcurves.sh
+++ lightcurves.sh
@@ -1,11 +1,20 @@
 # This bash script runs the three programs in order.
 
+#echo 'Enter the name of your dataset:'
+#read dataset
+
 echo 'Running primary.py ...'
+sleep 0.5
 python primary.py
+
 echo 'Primary eclipse values stored. Running secondary.py ...'
 sleep 1.5
 python secondary.py
+
 echo 'Secondary eclipse values stored. Running results.py ...'
 sleep 1.5
+
 python results.py
-echo 'Done. Results have been stored in finaloutput.txt.'
+echo 'Done. Results have been stored in an output file.'
+
+
